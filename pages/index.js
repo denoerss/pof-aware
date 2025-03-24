7423485af791332e3aef52cfa9bf033883de4b5b
denoerss <denise-albrecht@posteo.de>
2025-03-24 13:16:36 +0100

    Add under construction message to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Coming Soon: An informatie Website about POF Syndrome</h1>
+      <p>
+        A website dedicated to providing hope and valuable information for
+        individuals affected by POF syndrome, their families, and those
+        interested in learning more is currently under construction.
+        <br />
+        The goal is to create a space where knowledge, understanding, and
+        community can thrive, offering essential resources and stories of
+        strength. Thank you for your patience!
+      </p>
     </div>
   );
 }
